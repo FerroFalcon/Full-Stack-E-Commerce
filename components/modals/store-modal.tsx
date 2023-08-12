8090ffedff64a51c094f452f1939abc66821af62
Jaideep Ghungharwal <jdghungharwal04@gmail.com>
2023-08-12 21:51:12 +0530

    form components (admin)

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,18 +1,48 @@
 "use client";
 
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal";
+import { Form, FormField } from "@/components/ui/form";
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
 
 export const StoreModal = () => {
-    const StoreModal = useStoreModal();
-    return(
-        <Modal
-            title="Create store"
-            description="Add a new store to manage products and categories"
-            isOpen={StoreModal.isOpen}
-            onClose={StoreModal.onClose}
-        >
-            Future Create Store Form
-        </Modal>
-    );
+  const StoreModal = useStoreModal();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+    // TODO: Create store
+  };
+
+  return (
+    <Modal
+      title="Create store"
+      description="Add a new store to manage products and categories"
+      isOpen={StoreModal.isOpen}
+      onClose={StoreModal.onClose}
+    >
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField></FormField>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </Modal>
+  );
 };
